78ac9bff45d086ff9aff34b39bec267eebe29aec
ShenYanjie <279804711@qq.com>
2025-03-20 22:29:18 +0800

    modify again in home computer

--- StudyWinform/Form1.Designer.cs
+++ StudyWinform/Form1.Designer.cs
@@ -34,7 +34,6 @@
             this.textAccount = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -43,9 +42,10 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("宋体", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.label1.ForeColor = System.Drawing.Color.Red;
-            this.label1.Location = new System.Drawing.Point(351, 20);
+            this.label1.Location = new System.Drawing.Point(468, 27);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(109, 44);
+            this.label1.Size = new System.Drawing.Size(145, 59);
             this.label1.TabIndex = 0;
             this.label1.Text = "登录";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -54,9 +54,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label2.Location = new System.Drawing.Point(147, 103);
+            this.label2.Location = new System.Drawing.Point(196, 137);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(96, 28);
+            this.label2.Size = new System.Drawing.Size(131, 38);
             this.label2.TabIndex = 1;
             this.label2.Text = "账号：";
             // 
@@ -64,62 +65,57 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label3.Location = new System.Drawing.Point(147, 163);
+            this.label3.Location = new System.Drawing.Point(196, 217);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(96, 28);
+            this.label3.Size = new System.Drawing.Size(131, 38);
             this.label3.TabIndex = 2;
             this.label3.Text = "密码：";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // textAccount
             // 
-            this.textAccount.Location = new System.Drawing.Point(231, 103);
+            this.textAccount.Location = new System.Drawing.Point(308, 137);
+            this.textAccount.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textAccount.Name = "textAccount";
-            this.textAccount.Size = new System.Drawing.Size(331, 28);
+            this.textAccount.Size = new System.Drawing.Size(440, 35);
             this.textAccount.TabIndex = 3;
             this.textAccount.TextChanged += new System.EventHandler(this.textAccount_TextChanged);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(231, 163);
+            this.textBox2.Location = new System.Drawing.Point(308, 217);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox2.Name = "textBox2";
             this.textBox2.PasswordChar = '*';
-            this.textBox2.Size = new System.Drawing.Size(331, 28);
+            this.textBox2.Size = new System.Drawing.Size(440, 35);
             this.textBox2.TabIndex = 4;
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.button1.Location = new System.Drawing.Point(331, 259);
+            this.button1.Location = new System.Drawing.Point(441, 345);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(155, 82);
+            this.button1.Size = new System.Drawing.Size(207, 109);
             this.button1.TabIndex = 5;
             this.button1.Text = "登录";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(636, 181);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(62, 18);
-            this.label4.TabIndex = 6;
-            this.label4.Text = "label4";
-            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label4);
+            this.ClientSize = new System.Drawing.Size(1067, 600);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textAccount);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -136,7 +132,6 @@
         private System.Windows.Forms.TextBox textAccount;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label4;
     }
 }
 
